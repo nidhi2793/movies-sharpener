f31f2ef7b037eb1f04475216bd0299bb3c17893f
nidhi2793 <MY_NAME@example.com>
2023-08-05 20:21:04 +0530

    api-retry

--- src/App.js
+++ src/App.js
@@ -2,15 +2,41 @@ import React, { useState } from "react";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
+let fetchMovieInterval = null;
+
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function fetchMoviesWithRetry() {
+    fetchMovieInterval = setInterval(async () => {
+      let response = await fetch("https://swapi.dev/api/film");
+      if (response.ok) {
+        clearInterval(fetchMovieInterval);
+        setMovies((await response.json()).results);
+        setIsLoading(false);
+      }
+    }, 5000);
+  }
 
   async function fetchMoviesHandler() {
     setIsLoading(true);
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
-    setMovies(data.results);
+    setError(null);
+    try {
+      let response = await fetch("https://swapi.dev/api/film");
+      if (!response.ok) {
+        fetchMoviesWithRetry();
+        throw new Error("Retrying....");
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  function handleRetry() {
+    clearInterval(fetchMovieInterval);
+
     setIsLoading(false);
   }
 
@@ -20,7 +46,13 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {isLoading && <span>Loading....</span>}
+        {isLoading && !error && <span>Loading....</span>}
+        {isLoading && error && (
+          <div>
+            <p>{error}</p> <button onClick={handleRetry}>Cancel</button>{" "}
+          </div>
+        )}
+
         <MoviesList movies={movies} />
       </section>
     </React.Fragment>
