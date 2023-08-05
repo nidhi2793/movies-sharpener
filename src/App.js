01da1f484e575e54e2c04a2ce631966853572924
nidhi2793 <MY_NAME@example.com>
2023-08-05 17:42:27 +0530

    loader

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
-
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films");
     const data = await response.json();
-
     setMovies(data.results);
+    setIsLoading(false);
   }
 
   return (
@@ -19,6 +20,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
+        {isLoading && <span>Loading....</span>}
         <MoviesList movies={movies} />
       </section>
     </React.Fragment>
